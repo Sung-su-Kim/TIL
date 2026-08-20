c608864062e7246306a7ce6558283007061be021
Sungsu Kim <sungsu@g.yju.ac.kr>
2026-08-20 22:10:10 +0900

    fix: 2026-08-20: Javaコメントの追加

--- 03-Java/Hero.java
+++ 03-Java/Hero.java
@@ -1,8 +1,20 @@
 public class Hero {
-    String name;
+    String name; // 該当クラスが保持する属性値を変数として宣言
     int hp;
 
     public void attack() {
         System.out.println(name + " attacks!");
     }
 }
+
+// ここに String[] args がない理由は、プログラムのエントリーポイント（開始点）である main メソッド専用だからです。
+
+// このファイルは外部（コマンドライン）から入力値を受け取るのではなく、main クラス側から受け取るため、記述する必要はありません。
+// もし外部（呼び出し元）から値を受け取って使いたい場合は、以下のような形式で記述します。
+
+// public void attack(String enemy) {
+//     System.out.println(name + "が " + enemy + "を攻撃した");
+// }
+
+// こうすることで、Main.java から呼び出す際に括弧（引数）の中でターゲットを指定できます。
+// 例: myHero.attack("サノス");
--- 03-Java/Main.java
+++ 03-Java/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        Hero myHero = new Hero();
+        Hero myHero = new Hero(); // コンストラクタの呼び出し
 
-        myHero.name = "ironMan";
+        // new Hero() - メモリ（RAM）の空き領域を探し、Heroクラスをそのまま複製して新規作成し、そのメモリアドレスを返します。
+
+        // newの実行時に、ヒープ（Heap）領域へHeroクラスのデータを保存できるオブジェクト（インスタンス）が生成されます。
+
+        myHero.name = "ironMan"; // 「.」は参照演算子であり、myHeroが指し示すオブジェクトのname変数にアクセスして値を代入します。
         myHero.hp = 100;
 
         myHero.attack();
@@ -11,10 +15,19 @@ public class Main {
         if (args.length == 0) {
             System.out.println("No arguments provided.");
 
-            return;
+            return; // メソッド内でreturnが実行されると、メソッドの実行が終了します。
         }
         System.out.println("Hero: " + args[0]);
 
         System.out.println("program end");
     }
 }
+
+// Javaプログラムを実行する際、ターミナルでコマンドを入力して実行した時点で、データがmainメソッドに渡されます。
+
+// 入力値が想定より多く渡される分には問題ありませんが、少ないとエラーが発生します。
+
+// 解決策：
+// - if文を使って想定された入力数になるようチェックする
+// - for文とlengthを活用してすべての入力値を処理する
+// - 三項演算子を用いて柔軟に対応する
